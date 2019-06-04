5bc8a1c0f9b3f6a20c2f68ab11e54d535c2ffd32
Daniel Yount <dyountmusic@icloud.com>
2019-06-03 20:40:25 -0700

    Initial Commit

---
+++ ListApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ListApp
+//
+//  Created by Daniel Yount on 6/3/19.
+//  Copyright © 2019 Daniel Yount. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
